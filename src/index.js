6e9e2c4f1045672111e53d3372fd96e6698ea6aa
yinhang <demohang@163.com>
2018-01-10 14:01:11 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
 import { BrowserRouter } from 'react-router-dom'
 import 'whatwg-fetch'
 import 'es6-promise'
-import readFun from './reducers'
 import { Provider } from 'react-redux'
 import './styles/reset.css'
 import registerServiceWorker from './registerServiceWorker'
 import Routers from './router'
 import './assets/fonts/iconfont.css'
 import './styles/animations.css'
+import configureStore from './store/configureStore'
 
-const store = createStore(readFun)
+const store = configureStore()
 const rootEl = document.getElementById('root')
 
 const render = () => ReactDOM.render(
